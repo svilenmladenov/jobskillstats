f7d3cf84738ed3fffc79ce6de54869d364c6e4a5
Svilen Mladenov <chooseyourname8807@gmail.com>
2025-06-16 01:36:41 +0300

    all info collected - next step is inserts creation

--- project.py
+++ project.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import json5
+import re
 
 # Base URL pattern
 base_url = 'https://dev.bg/company/jobs/devops/?_paged='
@@ -32,12 +34,14 @@ while True:
     job_urls.update(page_job_urls)
     page += 1
 
-# Initialize an empty list
-skills_all = []
+
 # Print all unique job ad URLs
 # print("\nExtracted Job Ad URLs:")
 for job_url in sorted(job_urls):
     print(".", end="", flush=True)
+
+    # Initialize an empty list
+    skills_all = []
 
     # Send a GET request to fetch the page content
     response = requests.get(job_url)
@@ -46,9 +50,78 @@ for job_url in sorted(job_urls):
     # Parse the HTML content
     soup = BeautifulSoup(response.text, 'html.parser')
 
+    #print blank row
+    print(f"")
+
     # Find the tech-stack section
     tech_stack_section = soup.find('div', class_='tech-stack')
+
+    # Find the date-posted section
+    date = soup.find('li', class_='date-posted')
+
+    # Find the location section
+    location_section = soup.find('div', class_='tags-wrap')
+
+    # Extract "София"
+    city = location_section.find('a')
+    city_text = city.get_text(strip=True) if city else None
+
+    # Extract "Hybrid"
+    hybrid = location_section.find('span', class_='suffix-hybrid')
+    hybrid_text = hybrid.get_text(strip=True) if hybrid else None
+
+    # Find the script section
+    # Find all <script> tags
+    script_tags = soup.find_all("script")
+
+    # Search for the one that contains 'the_data'
+    for script in script_tags:
+        script_text = script.get_text()
+        if "the_data" in script_text:
+            # Extract just the JS object assigned to the_data
+            match = re.search(r"the_data\s*=\s*(\{.*?\});", script_text, re.DOTALL)
+            if match:
+                js_obj = match.group(1)
+
+                try:
+                    # Parse with json5 to handle JS-like syntax
+                    the_data = json5.loads(js_obj)
+                    company_name = the_data.get("attributes", {}).get("companyName")
+                    print(f"Company Name:\n{company_name}\n")
+
+                    role = the_data.get("attributes", {}).get("jobSubType")
+                    print(f"Role:\n{role}\n")
+
+                    job_position = the_data.get("attributes", {}).get("jobPosition")
+                    print(f"jobPosition:\n{job_position}\n")
+
+                    job_id = the_data.get("attributes", {}).get("jobId")
+                    print(f"jobId:\n{job_id}\n")
+
+                    payment_type = the_data.get("attributes", {}).get("paymentType")
+                    print(f"paymentType:\n{payment_type}\n")            
+
+                    quality_score = the_data.get("attributes", {}).get("qualityScore")
+                    print(f"qualityScore:\n{quality_score}\n")     
+
+                    value = the_data.get("attributes", {}).get("value")
+                    print(f"value:\n{value}\n")  
+                except Exception as e:
+                    print("Failed to parse the_data:", e)
+            break
+
+        
+    print("URL: ")
     print(job_url)
+    print()
+
+    print("Date: ")
+    print(date.find('time')['datetime'])
+    print()
+
+    print("City:", city_text)
+    print("Work type:", hybrid_text)
+
     if tech_stack_section:
         badges_clickable = tech_stack_section.find_all('div', class_='component-square-badge has-image clickable')
         badges_non_clickable = tech_stack_section.find_all('div', class_='component-square-badge has-image')
@@ -59,15 +132,6 @@ for job_url in sorted(job_urls):
         skills_all = skills_all + skills
 
 
-        print("Extracted Tech Stack Skills:")
+        print("Skills:")
         for skill in skills:
             print(f"- {skill}")
-    # else:
-    #     print("Tech Stack section not found.")
-
-# print all collected skills
-# for skill in skills_all:
-#     print(f"{skill}")
-
-# for skill, count in skill_counts.most_common():
-#     print(f"{skill}: {count}   ({count/jobs_count*100:.2f}%)")
