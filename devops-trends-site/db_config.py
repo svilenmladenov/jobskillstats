3c52b811a0abb652adaba3d14d87346163b35876
svilenmladenov <61394352+svilenmladenov@users.noreply.github.com>
2026-03-03 18:17:45 +0200

    Update db_config.py - switch to env vars

--- devops-trends-site/db_config.py
+++ devops-trends-site/db_config.py
@@ -2,8 +2,9 @@ import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='MySvilen',
-        database='project'
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
